fc971e3944bc8f5fb5dd78d2c2c0279a9a90b24d
yas7745 <yas7745@naver.com>
2023-02-05 23:43:39 +0900

    Beakjoon Silver

---
+++ Beakjoon/Silver/4948.py
@@ -0,0 +1,18 @@
+# 에라토스테네스의 체 이용
+# 1. 2 * n 이하의 소수를 전부 구한다
+# 2.n 부터 갯수를 구한다
+N = 123456*2
+prime = [True] * N
+for i in range(2,int(N**0.5)+1): # N의 최대 약수가 제곱근 n 이하이므로 제곱근 n까지 검사
+    if prime[i]: # i가 소수인 경우
+        for j in range(2*i,N,i): # i이후 i의 배수들을 False 판정
+            prime[j] = False
+
+while True:
+    n = int(input())
+    cnt = 0
+    if n == 0: break
+    else:
+        for i in range(n+1,2*n+1):
+            if prime[i]: cnt+= 1 # 소수 개수 세기
+    print(cnt)
