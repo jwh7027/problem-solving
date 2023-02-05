51b729d1410bc8cf7e56e0148162016a11528654
yas7745 <yas7745@naver.com>
2023-02-06 00:06:14 +0900

    math함수 사용

--- Beakjoon/Silver/4948.py
+++ Beakjoon/Silver/4948.py
@@ -1,9 +1,10 @@
+import math
 # 에라토스테네스의 체 이용
 # 1. 2 * n 이하의 소수를 전부 구한다
 # 2.n 부터 갯수를 구한다
-N = 123456*2
+N = 123456*2 + 1
 prime = [True] * N
-for i in range(2,int(N**0.5)+1): # N의 최대 약수가 제곱근 n 이하이므로 제곱근 n까지 검사
+for i in range(2,int(math.sqrt(N))+1): # N의 최대 약수가 제곱근 n 이하이므로 제곱근 n까지 검사
     if prime[i]: # i가 소수인 경우
         for j in range(2*i,N,i): # i이후 i의 배수들을 False 판정
             prime[j] = False
