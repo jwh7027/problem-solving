4cd8da30014395fa5657a39d6eaafb65488641c0
yas7745 <yas7745@naver.com>
2023-02-06 00:07:59 +0900

    math함수 사용

--- Beakjoon/Silver/2581.py
+++ Beakjoon/Silver/2581.py
@@ -1,10 +1,12 @@
+import math
 m = int(input())
 n = int(input())
 a = []
 for i in range(m,n+1):
     if i == 1: # 1 제외
         continue
-    for j in range(2,i//2+1): # 검색 축소
+    #for j in range(2,i//2+1):
+    for j in range(2,int(math.sqrt(i))+1): # 검색 축소
         if i % j == 0:
             break
     else:
