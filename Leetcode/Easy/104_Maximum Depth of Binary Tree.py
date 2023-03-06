304080ae3eda8ae4b48ea9a0f653996cf4cc566d
yas7745 <yas7745@naver.com>
2023-03-06 21:32:14 +0900

    add BFS solutions

--- Leetcode/Easy/104_Maximum Depth of Binary Tree.py
+++ Leetcode/Easy/104_Maximum Depth of Binary Tree.py
@@ -1,3 +1,4 @@
+#dfs 풀이
 class Solution:
     def maxDepth(self, root):
         max_depth = 0 
@@ -8,4 +9,21 @@ class Solution:
         right = self.maxDepth(root.right)
 
         max_depth = max(left,right) + 1
+        return max_depth
+#bfs 풀이
+from collections import deque
+class Solution:
+    def maxDepth(self,root):
+        max_depth = 0
+        if root is None:
+            return 0
+        q = deque()
+        q.append((root,1))
+        while q:
+            cur_node,cur_depth = q.popleft()
+            max_depth = cur_depth
+            if cur_node.left:
+                q.append((cur_node.left,cur_depth + 1))
+            if cur_node.right:
+                q.append((cur_node.right,cur_depth + 1))
         return max_depth
