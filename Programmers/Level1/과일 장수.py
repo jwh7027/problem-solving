ac3dfb2a5a26a08eb0c1fa6ab6d18d2758db9105
yas7745 <yas7745@naver.com>
2023-03-21 13:17:41 +0900

    Level1

--- Programmers/Level1/과일 장수.py
+++ Programmers/Level1/과일 장수.py
@@ -4,8 +4,19 @@ def solution(k, m, score):
     result = 0
     for i in range(0,len(score),m):
         answer.append(score[i:m+i])
-    for i in range(len(answer)):
-        if len(answer[i]) == m:
-            result += min(answer[i]) * m
+    for apple in answer:
+        if apple == m:
+            result += min(apple) * m
 
     return result
+#다른 풀이
+def solution(k, m, score):
+    score = sorted(score,reverse =True)
+    result = 0
+    for i in range(m-1,len(score),m):
+        result += score[i] * m
+    return result
+
+#다른 풀이
+def solution(k, m, score):
+    return sum(sorted(score)[len(score)%m::m])*m
