9436bb8e4ef8aed6538c9cf6a4f35468dfc016fb
yas7745 <yas7745@naver.com>
2023-04-03 14:26:00 +0900

    string

---
+++ Programmers/Level2/영어 끝말잇기.py
@@ -0,0 +1,8 @@
+def solution(n, words):
+    answer = [words[0][0]]
+    for i,w in enumerate(words):
+        if w not in answer and answer[-1][-1] == w[0]:
+            answer.append(w)
+        else:
+            return [i % n + 1, (i//n) + 1]
+    return [0,0]
