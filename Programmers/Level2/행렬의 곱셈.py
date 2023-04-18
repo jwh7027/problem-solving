b2c8757de307df3ab9099c9885a0a77bccc73dc4
yas7745 <yas7745@naver.com>
2023-04-18 12:45:14 +0900

    Level2

---
+++ Programmers/Level2/행렬의 곱셈.py
@@ -0,0 +1,15 @@
+import numpy as np
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        arr = []
+        for j in range(len(arr2[0])):
+            tmp = 0
+            for k in range(len(arr1[0])):
+                tmp += arr1[i][k] * arr2[k][j]
+            arr.append(tmp)
+        answer.append(arr)
+    return answer
+
+#numpy 풀이
+#   return (np.array(arr1)@np.array(arr2)).tolist()
